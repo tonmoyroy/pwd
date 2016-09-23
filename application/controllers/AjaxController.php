b6348cf6e06e4a69950a7ed4bbb5205712763e6c
Tonmoy <Tonmoy@DESKTOP-S8TPKTR>
2016-09-23 18:58:45 +0600

    File upload added

--- application/controllers/AjaxController.php
+++ application/controllers/AjaxController.php
@@ -1,10 +1,8 @@
 <?php
 
-class AjaxController extends Zend_Controller_Action
-{
+class AjaxController extends Zend_Controller_Action {
 
-    public function init()
-    {
+    public function init() {
         $this->PWDSession = new Zend_Session_Namespace('pwd');
         if (!$this->PWDSession->session_data) {
             $this->_redirect('index/login');
@@ -26,21 +24,71 @@ class AjaxController extends Zend_Controller_Action
         Zend_Registry::set('FISCAL_YR', $fiscal_year);
     }
 
-    public function indexAction()
-    {
+    public function indexAction() {
         // action body
     }
-    
-    public function paymentAction(){
+
+    public function paymentAction() {
         $this->_helper->layout->disableLayout();
-        $this->view->pay_method =$pay_meth= $this->_request->getPost('meth');
-        $this->view->total =$total= $this->_request->getPost('total');
-        $this->view->ca_no =$ca_no =$this->_request->getPost('ca_no');
+        $this->view->pay_method = $pay_meth = $this->_request->getPost('meth');
+        $this->view->total = $total = $this->_request->getPost('total');
+        $this->view->ca_no = $ca_no = $this->_request->getPost('ca_no');
         $this->view->bank = $this->ajax->getBanks();
         $this->view->paymentinfo = $payinfo = $this->ajax->getPaymentInfo($ca_no);
-       
     }
 
+    public function uploadfileAction() {
+        $this->_helper->layout->disableLayout();
+        $this->view->ca_no = $ca_no = $this->_request->getPost('ca_no');//print_r($ca_no);
+        $this->view->filelist =$file= $this->ajax->showFiles($ca_no);//print_r($file);exit;
+
+    }
+
+    public function storefileAction() {
+        $tmp[1] = $_FILES['file1']['tmp_name'];
+        $tmp[2] = $_FILES['file2']['tmp_name'];
+        $tmp[3] = $_FILES['file3']['tmp_name'];
+
+        $name[1] = $_FILES['file1']['name'];
+        $name[2] = $_FILES['file2']['name'];
+        $name[3] = $_FILES['file3']['name'];
+
+        $ca_no = $_POST['ca_no'];
+
+        if ($_FILES['file1']['size'] > 5242880 or $_FILES['file2']['size'] > 5242880 or $_FILES['file3']['size'] > 5242880) {
+            $errors[] = 'File size must be less than 5 MB';
+            echo $namef2;
+            exit;
+        }
+
+        if (empty($errors) == true) {
+
+            for ($i = 1; $i <= 3; $i++) {
+                if ($name[$i]) {
+                    $save_name = $ca_no . '_' . $i . '.pdf';
+                    $file_path = "documents/pay_security/" . $save_name;
+                    if (move_uploaded_file($tmp[$i], $file_path)) {
+                        $data['p_ca_no'] = $ca_no;
+                        $data['p_file_path'] = 'documents/pay_security/' . $save_name;
+                        $data['p_file_no'] = $i;
+                        $status = $this->ajax->updatefile($data);                                  //STORE FILENAME IN DATABASE
+
+                        if ($status['o_status_code'] == 1) {
+                            
+                        } else {
+                            unlink($file_path);
+                            $status['o_status_code'] = "SORRY!!FILE UPLOAD FAILED";
+                        }
+                    }
+                }
+            }
+            echo $status['o_status_message'];
+            exit;
+        } else {
+            echo $errors;
+            exit;
+        }
+    }
+   
 
 }
-
--- application/models/Ajax.php
+++ application/models/Ajax.php
@@ -29,4 +29,38 @@ class Application_Model_Ajax extends Zend_Db_Table {
         return $data;
     }
 
+    public function updatefile($data) {
+        $o_status_code = sprintf('%20f', '');
+        $o_status_message = sprintf('%4000s', '');
+        $out_parms = array(
+            "o_status_code" => &$o_status_code,
+            "o_status_message" => &$o_status_message
+        );
+
+        $all_data = array_merge($data, $out_parms);
+        //print_r($all_data);
+        
+        $this->_db->query("BEGIN NEW_FILE_UPLOAD (
+           :p_ca_no,
+           :p_file_path,
+           :p_file_no,
+           :o_status_code,
+           :o_status_message); END;", $all_data);
+        //var_dump ($all_data);exit;
+        return $all_data;
+    }
+    
+    public function showFiles($ca_no){
+        $sql = "SELECT FILE_ID,
+                CA_NO,
+                FILE_PATH,
+                FILE_NO,
+                C_DATE
+           FROM FILE_UPLOAD
+          WHERE CA_NO = '$ca_no' ORDER BY FILE_NO";
+        $data = $this->_db->fetchAll($sql);
+        //echo $sql;exit;
+        return $data;
+    }
+
 }
